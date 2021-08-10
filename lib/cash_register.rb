f4f1b60a8fc8241e82245e364e73cad2098731e9
Jelena <jelena.durovic@powerhrg.com>
2021-08-10 18:42:59 -0400

    commit tree eb9071deb940570a2c9d4dd39f6b770343996bf8

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+class CashRegister
 
+    attr_accessor :total, :discount, :price, :past_transactions, :title
+ 
+
+  def initialize(discount=nil, total=0)
+    @total = total
+    @discount = discount 
+    @title = []
+  end
+
+  def add_item(title, price, quantity = 1)
+   self.total = @total + (price * quantity)
+   @past_transactions = []
+   @past_transactions << price * quantity
+   quantity.times {@title << (title)}
+  
+  end 
+
+  def apply_discount
+   if  @discount 
+    self.total = self.total - (self.total * (@discount /100.0)).to_i
+    "After the discount, the total comes to $#{self.total}." 
+   else 
+     "There is no discount to apply."
+  end 
+end 
+
+def items
+    @title
+   
+end 
+
+def void_last_transaction
+    @past_transactions.empty? ? self.total = 0.0 :  self.total = @total - @past_transactions.pop
+    
+end 
+   
+
+end 
